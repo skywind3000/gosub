270e9fee2649cdda124787c1305b0ec9642313aa
skywind3000 <skywind3000@163.com>
2024-03-29 02:34:09 +0800

    update dummy.go

--- dummy.go
+++ dummy.go
@@ -4,3 +4,16 @@ func DummyAdd(x int, y int) int {
 	z := 100
 	return x + y + z
 }
+
+func DummySub(x int, y int) int {
+	z := 100
+	return x - y - z
+}
+
+func DummyMul(x int, y int) int {
+	return x * y
+}
+
+func DummyDiv(x int, y int) int {
+	return x / y
+}
