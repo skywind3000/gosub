85b337906b3317f93451dbcf572b55d747714971
skywind3000 <skywind3000@163.com>
2024-04-01 23:37:50 +0800

    Refactored PacketData struct and added new methods for easier management of packet data.

--- packet/PacketData.go
+++ packet/PacketData.go
@@ -1,15 +1,68 @@
 package packet
 
 type PacketData struct {
-	capacity int
-	head     int
-	tail     int
-	data     []byte
+	head int
+	tail int
+	data []byte
 }
 
 func (self *PacketData) NewPacketData(size int, overhead int) {
-	self.capacity = size + overhead
+	var capacity int = size + overhead
 	self.head = overhead
 	self.tail = overhead
-	self.data = make([]byte, self.capacity)
+	self.data = make([]byte, capacity)
+}
+
+func (self *PacketData) requireHead(size int) bool {
+	if self.head < size {
+		return false
+	}
+	return true
+}
+
+func (self *PacketData) requireTail(size int) bool {
+	if self.tail+size > len(self.data) {
+		return false
+	}
+	return true
+}
+
+func (self *PacketData) GetSize() int {
+	return self.tail - self.head
+}
+
+func (self *PacketData) PushHead(p []byte) bool {
+	if !self.requireHead(len(p)) {
+		return false
+	}
+	self.head -= len(p)
+	copy(self.data[self.head:], p)
+	return true
+}
+
+func (self *PacketData) PushTail(p []byte) bool {
+	if !self.requireTail(len(p)) {
+		return false
+	}
+	copy(self.data[self.tail:], p)
+	self.tail += len(p)
+	return true
+}
+
+func (self *PacketData) PopHead(p []byte) bool {
+	if self.GetSize() < len(p) {
+		return false
+	}
+	copy(p, self.data[self.head:])
+	self.head += len(p)
+	return true
+}
+
+func (self *PacketData) PopTail(p []byte) bool {
+	if self.GetSize() < len(p) {
+		return false
+	}
+	self.tail -= len(p)
+	copy(p, self.data[self.tail:])
+	return true
 }
