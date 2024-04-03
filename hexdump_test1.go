89e0e46de2274d1be376b1f9e667dbc594556c9e
skywind3000 <skywind3000@163.com>
2024-04-03 22:39:52 +0800

    update utils.go

--- hexdump_test1.go
+++ hexdump_test1.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"unsafe"
 
 	"github.com/skywind3000/gosub/packet"
 )
 
-func main() {
+func test1() {
 	b := []byte{}
 	b = append(b, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 	b = append(b, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46)
@@ -16,4 +17,15 @@ func main() {
 	t := &b[2]
 	println(*t)
 	println(uintptr(unsafe.Pointer(t)))
+}
+
+func test2() {
+	s := "hello, world"
+	fmt.Printf("%T\n", s[1])
+	fmt.Printf("%v\n", s[1])
+	fmt.Printf("%T\n", s[1:3])
+}
+
+func main() {
+	test2()
 }
--- packet/utils.go
+++ packet/utils.go
@@ -1,8 +1,9 @@
 package packet
 
 func LuaHashRange(seed uint32, p []byte, start int, end int) uint32 {
-	var h uint32 = seed
-	var step int = ((start - end) >> 5) + 1
+	var size int = end - start
+	var step int = (size >> 5) + 1
+	var h uint32 = seed ^ uint32(size)
 	for i := start; i < end; i += step {
 		h = h ^ ((h << 5) + (h >> 2) + uint32(p[i]))
 	}
@@ -10,9 +11,9 @@ func LuaHashRange(seed uint32, p []byte, start int, end int) uint32 {
 }
 
 func LuaHashString(seed uint32, s string) uint32 {
-	var h uint32 = seed
 	var size int = len(s)
 	var step int = (size >> 5) + 1
+	var h uint32 = seed ^ uint32(size)
 	for i := 0; i < size; i += step {
 		h = h ^ ((h << 5) + (h >> 2) + uint32(s[i]))
 	}
@@ -24,20 +25,20 @@ func LuaHashSlice(seed uint32, p []byte) uint32 {
 }
 
 func LuaHashUInt8(seed uint32, x uint8) uint32 {
-	var h uint32 = seed
+	var h uint32 = seed ^ 1
 	h = h ^ ((h << 5) + (h >> 2) + uint32(x))
 	return h
 }
 
 func LuaHashUInt16(seed uint32, x uint16) uint32 {
-	var h uint32 = seed
+	var h uint32 = seed ^ 2
 	h = h ^ ((h << 5) + (h >> 2) + uint32(x&0xFF))
 	h = h ^ ((h << 5) + (h >> 2) + uint32((x>>8)&0xFF))
 	return h
 }
 
 func LuaHashUInt32(seed uint32, x uint32) uint32 {
-	var h uint32 = seed
+	var h uint32 = seed ^ 4
 	h = h ^ ((h << 5) + (h >> 2) + ((x >> 0) & 0xFF))
 	h = h ^ ((h << 5) + (h >> 2) + ((x >> 8) & 0xFF))
 	h = h ^ ((h << 5) + (h >> 2) + ((x >> 16) & 0xFF))
@@ -46,7 +47,7 @@ func LuaHashUInt32(seed uint32, x uint32) uint32 {
 }
 
 func LuaHashUInt64(seed uint32, x uint64) uint32 {
-	var h uint32 = seed
+	var h uint32 = seed ^ 8
 	h = h ^ ((h << 5) + (h >> 2) + uint32(x&0xFF))
 	h = h ^ ((h << 5) + (h >> 2) + uint32((x>>8)&0xFF))
 	h = h ^ ((h << 5) + (h >> 2) + uint32((x>>16)&0xFF))
