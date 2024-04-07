3aa84e091d144c40f07f5db1149d0e89d4ed9bbd
skywind3000 <skywind3000@163.com>
2024-04-07 15:18:38 +0800

    update hash.go

--- packet/hash.go
+++ packet/hash.go
@@ -1,5 +1,49 @@
 package packet
 
+var crc32Table [256]uint32
+
+func initCRC32Table() {
+	var polynomial uint32 = 0xEDB88320
+	for i := uint32(0); i < uint32(256); i++ {
+		var crc uint32 = i
+		for j := uint32(0); j < uint32(8); j++ {
+			if crc&1 != 0 {
+				crc = (crc >> 1) ^ polynomial
+			} else {
+				crc = crc >> 1
+			}
+		}
+		crc32Table[i] = crc
+	}
+}
+
+func CRC32Range(p []byte, start int, end int) uint32 {
+	var crc uint32 = 0xFFFFFFFF
+	for i := start; i < end; i++ {
+		index := (crc ^ uint32(p[i])) & 0xFF
+		crc = crc32Table[index] ^ (crc >> 8)
+	}
+	return crc ^ 0xFFFFFFFF
+}
+
+func CRC32(p []byte) uint32 {
+	return CRC32Range(p, 0, len(p))
+}
+
+func FastCRC32Range(p []byte, start int, end int) uint32 {
+	var size int = end - start
+	var step int = (size >> 5) + 1
+	var crc uint32 = 0xFFFFFFFF
+	for i := start; i < end; i += step {
+		crc = crc32Table[(crc^uint32(p[i]))&0xFF] ^ (crc >> 8)
+	}
+	return crc ^ 0xFFFFFFFF
+}
+
+func FastCRC32(p []byte) uint32 {
+	return FastCRC32Range(p, 0, len(p))
+}
+
 func LuaHashRange(seed uint32, p []byte, start int, end int) uint32 {
 	var size int = end - start
 	var step int = (size >> 5) + 1
