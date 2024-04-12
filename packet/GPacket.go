96b888f861b141441ee0fa7c3fc4a05fa5e0bb1a
skywind3000 <skywind3000@163.com>
2024-04-12 19:06:29 +0800

    update GPacket.go

---
+++ packet/GPacket.go
@@ -0,0 +1,29 @@
+// =====================================================================
+//
+// # GPacket.go - General Packet
+//
+// Last Modified: 2024/04/12 19:04:21
+//
+// =====================================================================
+package packet
+
+type GPacket struct {
+	Frame  FrameHeader
+	Atom   AtomHeader
+	Access AccessHeader
+	Data   PacketData
+}
+
+func NewGPacket(size int) *GPacket {
+	pkt := &GPacket{
+		Frame:  FrameHeader{},
+		Atom:   AtomHeader{},
+		Access: AccessHeader{},
+	}
+	pkt.Data.Init(size, 64)
+	return pkt
+}
+
+func (self *GPacket) Release() {
+	self.Data.Release()
+}
